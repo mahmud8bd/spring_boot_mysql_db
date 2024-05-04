30809acad0b53587b1180bbcd7861cb88a16fdd8
mahmud8bd <mahmud8bd@gmail.com>
2024-05-04 11:13:16 +0600

    04-05-24

---
+++ src/main/java/com/sazzad/mysql/SpringBootMysqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.sazzad.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlDbApplication.class, args);
+	}
+
+}
